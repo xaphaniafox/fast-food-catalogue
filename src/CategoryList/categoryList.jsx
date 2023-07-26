ff8c62c8e84b4bafab50f1dd93af741ee1c09ff6
xaphaniafox <f.hosseini64@gmail.com>
2023-07-26 22:17:40 +0300

    creating nav

--- src/CategoryList/categoryList.jsx
+++ src/CategoryList/categoryList.jsx
@@ -1,16 +1,39 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "../axios";
 
 const CategoryList = () => {
+  const [categories, setCategories] = useState([]);
   useEffect(() => {
     const fetchCategories = async () => {
       const response = await axios.get("/FoodCategory/categories");
-      console.log(response.data);
+      setCategories(response.data);
     };
     fetchCategories();
   }, []);
 
-  return <div>Category List</div>;
+  return (
+    <nav className="container mt-n5">
+      <div
+        className="d-flex align-items-center bg-white rounded-3 shadow-lg py-4"
+        style={{ height: "80px" }}
+      >
+        <ul className="nav">
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              همه فست فودها
+            </a>
+          </li>
+          {categories.map((category) => (
+            <li className="nav-item" key={category.id}>
+              <a className="nav-link" href="#">
+                {category.name}
+              </a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default CategoryList;
