10dca838aa83a722ec573539bc37b4c5a639ffd1
xaphaniafox <f.hosseini64@gmail.com>
2023-07-27 15:43:23 +0300

    FastFoodList component, fetching data

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,41 @@
+import { useEffect, useState } from "react";
+import axios from "./axios";
 import "./App.css";
 import CategoryList from "./CategoryList/categoryList";
 import Header from "./Header/header";
+import Loading from "./Loading/loading";
+import FastFoodList from "./FastFoodList/fastFoodList";
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [fastFoodItems, setFastFoodItems] = useState([]);
+
+  const fetchData = async (categoryId = null) => {
+    setLoading(true);
+    const response = await axios.get(
+      `/FastFood/list${categoryId ? "?categoryId=" + categoryId : ""}`
+    );
+    setLoading(false);
+    setFastFoodItems(response.data);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const renderContent = () => {
+    if (loading) {
+      return <Loading theme="dark" />;
+    }
+    return <FastFoodList fastFoodItems={fastFoodItems} />;
+  };
+
   return (
     <>
       <div className="wrapper bg-faded-dark">
         <Header />
         <CategoryList />
+        <div className="container mt-4">{renderContent()}</div>
       </div>
     </>
   );
---
+++ src/FastFoodList/fastFoodList.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FastFoodList = ({ fastFoodItems }) => {
+  return <div>Fast Food Items</div>;
+};
+
+export default FastFoodList;
