0fa2d220dc73e08c75480709b22600c638f453c4
xaphaniafox <f.hosseini64@gmail.com>
2023-07-28 03:09:10 +0300

    creating FastFoodItem component (mt-auto is not working. not the same height for cards

--- src/FastFoodList/fastFoodList.jsx
+++ src/FastFoodList/fastFoodList.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import FastFoodItem from "../FastFoodItem/fastFoodItem";
 
 const FastFoodList = ({ fastFoodItems }) => {
-  return <div>Fast Food Items</div>;
+  return (
+    <div className="row">
+      {fastFoodItems.map((fastFoodItem) => {
+        return (
+          <div
+            className="col-md-4 col-sm-6 mb-grid-gutter"
+            key={fastFoodItem.id}
+          >
+            <FastFoodItem {...fastFoodItem} />
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default FastFoodList;
