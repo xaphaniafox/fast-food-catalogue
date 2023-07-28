feb6fc4c077a43d6e713be3ab6d3603404742aa2
xaphaniafox <f.hosseini64@gmail.com>
2023-07-28 04:35:14 +0300

    fixing the same height cards problem with media queries

--- src/FastFoodItem/fastFoodItem.jsx
+++ src/FastFoodItem/fastFoodItem.jsx
@@ -1,15 +1,16 @@
 import React from "react";
 import { TiShoppingCart } from "react-icons/ti";
+import "./fastFoodItem.css";
 
 const FastFoodItem = ({ name, price, ingredients, imageUrl }) => {
   return (
     <div>
-      <div className="card product-card h-100 border-0 shadow-sm pb-1">
+      <div className="card-container card product-card h-100 border-0 shadow-sm pb-1">
         <span className="badge badge-end badge-shadow bg-success fs-md fw-medium">
           قیمت: {price.toLocaleString()} تومان
         </span>
         <img className="card-img-top" src={imageUrl} alt="fast food image" />
-        <div className="card-body text-center pt-3 pb-4 d-flex flex-column">
+        <div className="card-body text-center pt-3 pb-4">
           <h5 className="mb-2">{name}</h5>
           <div className="fs-ms fw-bold text-muted mb-3">{ingredients}</div>
           <button className="btn btn-outline-success btn-sm w-100 mt-auto fw-bold">
